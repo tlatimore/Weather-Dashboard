397af56cb39e5c1e8b379b7b3382c6ec4164c42b
tlatimore <tlatimore0551@yahoo.com>
2025-01-29 21:20:39 -0500

    added Develop

---
+++ Develop/server/src/service/historyService.ts
@@ -0,0 +1,65 @@
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
+
+class City {
+  name: string;
+  id: string;
+
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+class HistoryService {
+  private async read() {
+    return await fs.readFile('db/db.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    });
+  }
+
+  private async write(cities: City[]) {
+    return await fs.writeFile('db/db.json', JSON.stringify(cities, null, '\t'));
+  }
+
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+
+      try {
+        parsedCities = [].concat(JSON.parse(cities));
+      } catch (err) {
+        parsedCities = [];
+      }
+
+      return parsedCities;
+    });
+  }
+
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error('City is required');
+    }
+
+    const addedCity: City = { name: city, id: uuidv4() };
+
+    return await this.getCities()
+      .then((cities) => {
+        if (cities.find((index) => index.name === city)) {
+          return cities;
+        }
+        return [...cities, addedCity];
+      })
+      .then((updatedCities) => this.write(updatedCities))
+      .then(() => addedCity);
+  }
+
+  async deleteCity(id: string) {
+    return await this.getCities()
+      .then((cities) => cities.filter((city) => city.id !== id))
+      .then((filteredCities) => this.write(filteredCities));
+  }
+}
+
+export default new HistoryService();
